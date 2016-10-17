3881f3508eaf50e357360d9c331c9e774c8c63d9
Joshua Marquez <joshua.marquez.n@gmail.com>
2016-10-17 00:18:53 -0700

    new way to load model method

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 'use strict';
 
+const _ = require('lodash');
+
 module.exports = function(sails) {
+
+  /**
+   * Checks for the existence of the record in the first parameter.
+   * If it can't be found, the record in the second parameter is created,
+   * otherwise it is updated with the second parameter.
+   *
+   * @param  {Object}   criteria The criteria used to find the record.
+   *                             If not found and no 'values' is provided,
+   *                             it is also the record that will be created.
+   * @param  {Object}   values   The object that you would like to update or
+   *                             create.
+   * @return {Promise}
+   */
+  function updateOrCreate(criteria, values) {
+    return new Promise((resolve, reject) => {
+      if (!values) {
+        values = criteria.where ? criteria.where : criteria;
+      }
+
+      this.findOne(criteria)
+      .then((result) => {
+        if (result) {
+          return this.update(criteria, values);
+        }
+
+        return this.create(values);
+      })
+      .then((data) => {
+        if (Array.isArray(data)) {
+          data = _.first(data);
+        }
+
+        return data;
+      })
+      .then(resolve)
+      .catch(reject);
+    });
+  }
 
   return {
 
@@ -10,57 +50,16 @@ module.exports = function(sails) {
     defaults: {
 
       __configKey__: {
-        name: 'updateOrCreateHook'
+        name: 'update-or-create'
       }
     },
 
-    /**
-     * Initialize the hook
-     *
-     * @param  {Function} cb Callback for when we're done initializing.
-     */
-    initialize: function(cb) {
-      /**
-       * Checks for the existence of the record in the first parameter.
-       * If it can't be found, the record in the second parameter is created,
-       * otherwise it is updated with the second parameter.
-       *
-       * @param  {Object}   criteria The criteria used to find the record.
-       *                             If not found and no 'values' is provided,
-       *                             it is also the record that will be created.
-       * @param  {Object}   values   The object that you would like to update or
-       *                             create.
-       * @return {Promise}
-       */
-      sails.config.models.updateOrCreate = function(criteria, values) {
-        return new Promise((resolve, reject) => {
-          if (!values) {
-            values = criteria.where ? criteria.where : criteria;
-          }
-
-          this.findOne(criteria)
-          .then((result) => {
-            if (result) {
-              return this.update(criteria, values);
-            }
-
-            return this.create(values);
-          })
-          .then((data) => {
-            if (Array.isArray(data)) {
-              data = data[0];
-            }
-
-            return data;
-          })
-          .then(resolve)
-          .catch(reject);
-        });
-      };
-
-      // We're done initializing.
-      return cb();
+    configure: function() {
+      // This config will make `updateOrCreate` method
+      // available in all models.
+      sails.config.models.updateOrCreate = updateOrCreate;
     }
+
   };
 
 };
